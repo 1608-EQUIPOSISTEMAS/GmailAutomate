2e86298892a276e148c059abc939ba976aedcb30
1608-EQUIPOSISTEMAS <sistemasbd.we@gmail.com>
2026-03-18 11:53:32 -0500

    v2.2

--- tests/config.js
+++ tests/config.js
@@ -1,4 +1,6 @@
+const host = process.env.API_HOST || 'http://localhost:4111';
+
 module.exports = {
-    BASE:  'http://localhost:4111/api',
+    BASE:  `${host}/api`,
     TOKEN: process.env.API_SECRET || 'CLAVE_SEGURA_WE_2026',
 };
